6b893289124240192c571d5cbda09ba7b2e3ef0e
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-01-28 10:06:41 +0100

    Klassen

---
+++ jan25playground/src/main/java/de/placeholder/PackageTest.java
@@ -0,0 +1,39 @@
+package de.placeholder; //Nur Kleinbuchstaben und Punkte
+
+//Alle Klassen aus java.lang sind automatisch importiert.
+//Alle anderen Klassen müssen importiert werden, wenn sie in einem anden Package liegen.
+
+//import java.until.Date;
+import java.util.*; //Importiert alle Klassen aus java.until
+
+//End-Of-Line Kommentar: Alles wird bis zum ENde der Zeile auskommentiert.
+
+//Deklaration = Reservieren eines Namens
+//Eine Klasse, die nicht public ist, kann nur im gleichen Package verwendet werden.
+public class PackageTest { //PascalCase: beginnt mit Großbuchstaben.
+
+    public static void main(String[] args) {
+
+        //Deklaration und Initialisierung (Zuweisung eines Startwertes)
+        int zahl = 10;
+
+        int zahl2; //Deklaration (Der Name der Variable ist reserviert)
+        zahl2 = 15; //Initialisierung (Erste Wertzuweisung)
+        zahl2 = 20; //Änderung
+
+        //int zahl2; // Error: Name ist bereits reserviert
+
+        System.out.println(new PackageTest());
+
+        Date heute = new Date();
+        System.out.println(heute);
+    }
+
+    //Inhalt der Umgebenden Klasse
+    class InnereKlasse {
+    }
+}
+
+//Ohne Zugriffsmodifikator ist die Klasse package-private (nur im Package sichtbar)
+class ZweiteKlasse {
+}
