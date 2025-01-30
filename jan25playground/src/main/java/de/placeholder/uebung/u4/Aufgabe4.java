15a413bfe6cf8c20f78bacac38e7dabe65a20dfe
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-01-30 15:46:00 +0100

    Aufgabenstellung

---
+++ jan25playground/src/main/java/de/placeholder/uebung/u4/Aufgabe4.java
@@ -0,0 +1,44 @@
+package de.placeholder.uebung.u4;
+
+public class Aufgabe4 {
+
+    /* 1
+    Deine Aufgabe ist es, ein einfaches Programm zu schreiben,
+    das Minuten in Sekunden umwandelt. Das bedeutet, wenn dir jemand eine Zahl
+    in Minuten nennt, wird dein Programm ihnen sagen, wie viele Sekunden das sind.
+     */
+
+    /* 2
+    Schreibe ein Programm, das prüft, ob eine Zahl gerade oder ungerade ist.
+    Der Benutzer gibt eine Zahl ein, und das Programm teilt ihm mit, ob die Zahl gerade
+    oder ungerade ist.
+     */
+
+    /* 3
+    Schreibe ein Programm, das basierend auf dem Alter des Benutzers eine
+    Alterskategorie zurückgibt. Der Benutzer gibt sein Alter ein, und das
+    Programm kategorisiert ihn als Kind, Teenager, Erwachsener oder Senior.
+
+    Das Programm fragt nach einer Ganzzahl. (int).
+    Verwende eine if-else-Struktur, um die richtige Alterskategorie zu bestimmen:
+        a. 0–12: Kind
+        b. 13–19: Teenager
+        c. 20–64: Erwachsener
+        d. 65 und älter: Senior
+    Teile dem Benutzer seine Alterskategorie mit.
+    Prüfe alle möglichen Fälle und Grenzen, ob dein Programm richtig funktioniert.
+    (Mit Grenzen sind exakt 0, 12, 13, 19 usw. gemeint)
+     */
+
+    /* 4
+    Schreibe ein Programm, das basierend auf dem Alter des Benutzers einen Rabatt
+    berechnet und diesen auf einen eingegebenen Preis anwendet.
+    Personen unter 18 Jahren erhalten 20% Rabatt, während alle
+    anderen 10% Rabatt bekommen.
+
+    Das Programm fragt das Alter des Benutzers ab.
+    Das Programm fragt nach dem Preis eines Produkts. Welcher Datentyp ist hierfür passend?
+    Berechne den finalen Preis nach Anwendung des Rabatts.
+    Teile dem Benutzer den Rabatt und den Endpreis mit.
+     */
+}
