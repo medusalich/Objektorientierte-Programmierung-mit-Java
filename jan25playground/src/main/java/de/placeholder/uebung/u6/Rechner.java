19e9bb4b8c627965368485e8a2ea48d75502b5fa
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-03 15:28:24 +0100

    Taschenrechner mit 4 Rechenarten Aufgabe

--- jan25playground/src/main/java/de/placeholder/uebung/u6/Rechner.java
+++ jan25playground/src/main/java/de/placeholder/uebung/u6/Rechner.java
@@ -1,4 +1,20 @@
 package de.placeholder.uebung.u6;
 
 public class Rechner {
+
+    public static double addieren( double zahl1, double zahl2){
+        return zahl1 + zahl2;
+    }
+
+    public static double subtrahieren( double zahl1, double zahl2){
+        return zahl1 - zahl2;
+    }
+
+    public static double multiplizieren( double zahl1, double zahl2){
+        return zahl1 * zahl2;
+    }
+
+    public static double dividieren( double zahl1, double zahl2){
+        return zahl1 / zahl2;
+    }
 }
--- jan25playground/src/main/java/de/placeholder/uebung/u6/RechnerTest.java
+++ jan25playground/src/main/java/de/placeholder/uebung/u6/RechnerTest.java
@@ -1,8 +1,43 @@
 package de.placeholder.uebung.u6;
 
+import java.util.Scanner;
+
 public class RechnerTest {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        System.out.println("Gib deine erste Zahl ein: ");
+        double zahl1 = scanner.nextDouble();
+
+        System.out.println("Gib deine zweite Zahl ein: ");
+        double zahl2 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("\nWählen Sie die Operation (addieren, subtrahieren, multiplizieren, dividieren): ");
+        String waehlen = scanner.nextLine().toLowerCase();
+
+        double ergebnis = 0;
+
+        switch(waehlen) {
+            case "addieren":
+                ergebnis = Rechner.addieren(zahl1, zahl2);
+                System.out.println("Ergebnis der Addition: " + ergebnis);
+                break;
+            case "subtrahieren":
+                ergebnis = Rechner.subtrahieren(zahl1, zahl2);
+                System.out.println("Ergebnis der Subtraktion: " + ergebnis);
+                break;
+            case "multiplizieren":
+                ergebnis = Rechner.multiplizieren(zahl1, zahl2);
+                System.out.println("Ergebnis der Addition: " + ergebnis);
+                break;
+            case "dividieren":
+                ergebnis = Rechner.dividieren(zahl1, zahl2);
+                System.out.println("Ergebnis der Addition: " + ergebnis);
+                break;
+        }
 
     }
 }
