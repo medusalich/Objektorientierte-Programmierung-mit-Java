3670f899a3ab5a4414f95ed314f8a89367128662
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-03 12:05:09 +0100

    Arrays

--- jan25playground/src/main/java/de/placeholder/arrays/Todos.java
+++ jan25playground/src/main/java/de/placeholder/arrays/Todos.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 
 public class Todos {
 
+    //Soll eine Variable in allen Klassen-Methoden verfügbar sein, dann muss sie
+    //als Klassen-Variable deklariert sein.
+
+    //Variablen außerhalb der Methoden, die NICHT static sind, sind Instanzvariablen.
+    //Sie gehören einem Objekt. Vor der Benutzung muss new zuerst ein Objekt erzeugt werden.
+
+    //private = Diese Variable ist nur INNERHALB dieser Klasse sichtbar.
+    //static = Diese Variable gehört der Klasse.
+    //finale = Variable kann sich nicht ändern.
+    //int = Datentyp
+    //SIZE = Variablenname
     private static final int SIZE = 10; //Klassenvariable: Scope: In allen Methoden dieser Klasse sichtbar.
 
+    //Es bleibt IMMER ein String-Array mit Größe 10, Inhalt darf sich ändern.
     private static String[] todos = new String[SIZE]; //Array mit 10 Postionen.
 
     private static int next = 0;
@@ -16,20 +28,30 @@ public class Todos {
 
         // final int SIZE = 10; //Lokale Konstante: Scope = nur die Main-Methode
 
-        while(true) { //Endlosschleife
-            if(next == todos.length) {
-                System.out.println("Todolist ist voll!");
-                break; //Abbrechen, wenn das Array voll ist.
+        app: while(true) { //Endlosschleife
+
+            System.out.println("\nAuswahl: ");
+            String auswahl = scanner.nextLine().toLowerCase();
+            switch(auswahl){
+                case "create" -> createTodo();
+                case "print" -> printTodos(); //Array Ausgaben
+                case "exit" -> {
+                    break app; //bricht die while-Schleife ab
+                }
+                default -> System.out.println("Falsche Eingabe!");
             }
-
-            createTodo();
-
-            printTodos(); //Array Ausgaben
         }
-
+        System.out.println("Ende!");
     }
 
     private static void createTodo(){
+
+        if(next == todos.length) {
+            System.out.println("Todolist ist voll!");
+            //break; //Ist für Schleifen und switch gedacht
+            return; //Abbrechen, wenn das Array voll ist.
+        }
+
         System.out.println("Todo: ");
         String eingabe = scanner.nextLine(); //Liest die ganze Zeile ein ikl. Leerzeichen.
         todos[next] = eingabe; //Wert von der Konsole wird im Array auf der Position 0 abgelegt.
