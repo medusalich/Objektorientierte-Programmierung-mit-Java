5b80c8b157e6879dc8b3c7d496f34fff5ea018b5
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-12 14:28:50 +0100

    Equals & HashCode

---
+++ jan25playground/src/main/java/de/placeholder/uebung/u14/Film.java
@@ -0,0 +1,27 @@
+package de.placeholder.uebung.u14;
+
+import java.util.Objects;
+
+public class Film {
+    private String titel;
+    private String genre;
+
+    public Film(String titel, String genre) {
+        this.titel = titel;
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return Objects.equals(titel, film.titel) && Objects.equals(genre, film.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titel, genre);
+    }
+}
+
+
---
+++ jan25playground/src/main/java/de/placeholder/uebung/u14/Kunde.java
@@ -0,0 +1,43 @@
+package de.placeholder.uebung.u14;
+
+import java.util.Objects;
+
+public class Kunde {
+    private String name;
+    private String email;
+
+    public Kunde(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Kunde kunde = (Kunde) o;
+        return Objects.equals(name, kunde.name) && Objects.equals(email, kunde.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email);
+    }
+
+    public static void main(String[] args) {
+
+        Kunde kunde1 = new Kunde("Bruce", "bruce@example.com");
+        Kunde kunde2 = new Kunde("Peter", "peter@example.com");
+        System.out.println("Kundenvergleich: " + kunde1.equals(kunde2));
+        System.out.println("Kunden Hashcode: " + (kunde1.hashCode() == kunde2.hashCode()));
+
+        Film film1 = new Film("Avangers", "Action");
+        Film film2 = new Film("Avangers", "Action");
+        System.out.println("Filmvergleich: " + film1.equals(film2));
+        System.out.println("Film Hashcode: " + (film1.hashCode() == film2.hashCode()));
+    }
+}
+
+
+
+
+
