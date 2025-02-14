195a7825b85f1e5895626dda481a63f3ddb40ab6
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-14 15:17:29 +0100

    Aufgabe

---
+++ jan25playground/src/main/java/de/placeholder/uebung/u16/Aufgabe16.java
@@ -0,0 +1,48 @@
+package de.placeholder.uebung.u16;
+
+public class Aufgabe16 {
+
+    /*
+    Aufgabe 1
+    Schreibe ein Programm für eine Kaffeemaschine.
+
+    Über die Konsole kann ein Produkt ausgewählt werden.
+    Daraufhin produziert das Programm ein Objekt einer Kindklasse der Klasse Kaffeeprodukt.
+
+    Für den Kaffee soll ausreichend Geld eingeworfen werden.
+    Das passiert über eine Methode der Kaffeemaschine, wo man als Parameter einen double
+    übergibt. Sollte dies nicht der Falls sein, bekommt man eine entsprechende Meldung.
+
+    Erweiterung:
+    Protokolliere im Hintergrund den Bezug der Kaffeeprodukte. Das Protokoll kann abgefragt werden.
+    */
+
+    /*
+    Aufgabe 2
+    Schreibe ein Kommandozeilen-Programm welches eine Sammlung von Produkten verwalten kann.
+    Jedes Produkt soll ein Objekt sein, welches Datum (Aufnahme in den Bestand), Name,
+    Beschreibung, Menge und Preis als Eigenschaften enthält.
+
+    Folgende Aktionen sollen möglich sein:
+    1. Erstellen eines neuen Produkts
+    2. Ausgabe aller vorhandener Produkte
+    3. Sortierte Ausgabe (nach Aufnahme in den Bestand)
+
+    Bei der Ausgabe soll das Datum in landesüblicher Schreibweise ausgegeben werden.
+    Die Daten sollen nicht dauerhaft gespeichert werden d.h. nach der Beendigung des
+    Programms gehen alle eingegeben Daten verloren.
+
+    Erweiterung:
+    Implementiere eine Klasse VerderblichesProdukt.
+    Diese Klasse soll von Produkt erben und es um ein Verfallsdatum erweitern.
+
+    Die Benutzeroberfläche soll so erweitert werden, dass der Benutzer entscheiden kann,
+    ob er ein „normales“ Produkt oder ein VerderblichesProdukt erfassen möchte.
+
+    Die Ausgabe verderblicher Produkte soll das Verfallsdatum enthalten.
+
+    Erweiterung2:
+    Erweitere das Programm so, dass eine Suche nach Produkten möglich ist, die innerhalb eines
+    bestimmten Zeitraums in den Bestand aufgenommen wurden.
+    */
+}
