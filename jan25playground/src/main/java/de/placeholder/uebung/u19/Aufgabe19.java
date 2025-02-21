0dd90ae8f47fd3983a5560ccfcb07a98507924bc
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-21 10:00:15 +0100

    JavaFX

---
+++ jan25playground/src/main/java/de/placeholder/uebung/u19/Aufgabe19.java
@@ -0,0 +1,12 @@
+package de.placeholder.uebung.u19;
+
+public class Aufgabe19 {
+
+    /*
+    Schreibe ein kleines GUI-Programm mit JavaFX.
+    Es sollen Personen erfasst werden können.
+    Jede Person hat einen Vor- und Nachnamen, sowie ein alter.
+    Nach jedem Speichern wird die Liste der Personen aktualisiert.
+     */
+
+}
