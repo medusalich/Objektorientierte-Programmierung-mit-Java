6b773c8d05f324e6749e5e8c4ad3d2570dd136da
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-24 13:46:10 +0100

    Patterns: JDBC

--- jan25playground/src/main/java/de/placeholder/datenbanken/VerbindungsTest.java
+++ jan25playground/src/main/java/de/placeholder/datenbanken/VerbindungsTest.java
@@ -1,14 +1,49 @@
 package de.placeholder.datenbanken;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VerbindungsTest {
 
     public static void main(String[] args) {
 
-         datenAbfragen();
+        datenAbfragen();
 
 //        datenSchreiben();
+
+//        datenLoeschen();
+
+//        datenAendern();
+    }
+
+    private static void datenAendern() {
+        try (Connection verbindung = DbUtility.getConnection();
+             Statement stmt = verbindung.createStatement()) {
+
+            final String SQL = "UPDATE kunden SET vorname = 'Scott', nachname = 'Lang', aktiv = 1 WHERE id = 2";
+
+            if (stmt.executeUpdate(SQL) == 1) {
+                System.out.println("Geändert.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void datenLoeschen() {
+        try (Connection verbindung = DbUtility.getConnection();
+             Statement stmt = verbindung.createStatement()) {
+
+            final String SQL = "DELETE FROM kunden WHERE id = 3";
+
+            if (stmt.executeUpdate(SQL) == 1) {
+                System.out.println("Gelöscht.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static void datenSchreiben() {
@@ -17,11 +52,10 @@ public class VerbindungsTest {
 
             final String SQL = "INSERT INTO kunden (vorname, nachname) VALUES('Tony', 'Stark')";
 
-            if(stmt.executeUpdate(SQL) > 0) {
+            if (stmt.executeUpdate(SQL) > 0) {
                 System.out.println("Gespeichert.");
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -42,11 +76,12 @@ public class VerbindungsTest {
             while (daten.next()) {
                 // Daten werden mit passender Methode pro Spalte abgefragt.
                 // Passende Methode muss zu dem jeweiligen Datentyp der Spalte passen.
-                System.out.println(daten.getString("vorname") + " " + daten.getString("nachname"));
+                System.out.println(daten.getInt("id") + " " +
+                        daten.getString("vorname") + " " +
+                        daten.getString("nachname") + ", "+
+                        (daten.getBoolean("aktiv") ? "aktiv" : "nicht aktiv"));
             }
-
             // Verbindung und stmt werden automatisch geschlossen.
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
